e104986850f7149223706de304c7d8f34b3d455c
Liena Vu <liena_vu@epam.com>
2020-05-22 10:22:04 +0300

    test methods

--- test/scope-and-context.spec.js
+++ test/scope-and-context.spec.js
@@ -38,45 +38,42 @@ describe('Fighter', function() {
     expect( console.log.calledOnce ).to.be.true;
     expect( console.log.calledWith(`Name:Max,Wins:0,Losses:1`)).to.be.true;
   });
-  
 
-  it('should add health points',()=>{
-    let startHealth=fighter1.getHealth();
+
+  it('should add health points', ()=>{
+    const startHealth=fighter1.getHealth();
     fighter1.heal(10);
     expect(fighter1.getHealth()).to.equal(startHealth+10);
-
-  })
+  });
 
   it('should decrease health points by 100', function() {
-    let startHealth=fighter1.getHealth();
-    fighter1.dealDamage(100)
+    const startHealth=fighter1.getHealth();
+    fighter1.dealDamage(100);
     expect(fighter1.getHealth()).to.equal(startHealth-100);
   });
 
 
   it('health points should equal 0', function() {
-    fighter1.dealDamage(200)
+    fighter1.dealDamage(200);
     expect(fighter1.getHealth()).to.equal(0);
   });
 
-  it('should log a message',()=>{
-    let startHealth= fighter2.getHealth();
-    fighter1.attack(fighter2)
-    if (startHealth===fighter2.getHealth()){
+  it('should log a message', ()=>{
+    const startHealth= fighter2.getHealth();
+    fighter1.attack(fighter2);
+    if (startHealth===fighter2.getHealth()) {
       expect( console.log.calledOnce ).to.be.true;
       expect( console.log.calledWith(`Max attack missed`)).to.be.true;
-    } else{
+    } else {
       expect( console.log.calledOnce ).to.be.true;
       expect( console.log.calledWith(`Maximus makes 25 damage to Max`)).to.be.true;
     }
-  })
+  });
 
   it('there should be a loser', ()=>{
     const loser=battle(fighter2, fighter3);
     expect(loser).to.equal((winnerExists(fighter2, fighter3)?isLoser(fighter2, fighter3):0));
   });
-
-  
 });
 
 const winnerExists=(fighter1, fighter2)=>{
