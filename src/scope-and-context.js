ef549099e9f79ed47e1999f51b74061b6e229ec1
Mariia Korzun <3151020m@gmail.com>
2020-11-29 00:24:56 +0200

    HW scope and context is done.

--- src/scope-and-context.js
+++ src/scope-and-context.js
@@ -1,10 +1,109 @@
-function Fighter() {
-  // add an implementation here
-  
+class Fighter {
+  #fighterName
+  #fighterDamage
+  #fighterHealth
+  #fighterStrength
+  #fighterAgility
+  #fighterWin
+  #fighterLoss
+  constructor(object) {
+    this.#fighterName = object.name
+    this.#fighterDamage = object.damage
+    this.#fighterHealth = object.hp
+    this.#fighterStrength = object.strength
+    this.#fighterAgility = object.agility
+    this.#fighterWin = 0
+    this.#fighterLoss = 0
+  }
+  getName() {
+    return this.#fighterName
+  }
+
+  getDamage() {
+    return this.#fighterDamage
+  }
+
+  getStrength() {
+    return this.#fighterStrength
+  }
+
+  getAgility() {
+    return this.#fighterAgility
+  }
+
+  getHealth() {
+    return this.#fighterHealth
+  }
+
+  getWin() {
+    return this.#fighterWin
+  }
+
+  addWin() {
+    this.#fighterWin++
+  }
+
+  getLoss() {
+   return this.#fighterLoss
+  }
+
+  addLoss() {
+    this.#fighterLoss++
+  }
+
+  heal(health) {
+    this.#fighterHealth += health
+  }
+
+  dealDamage(health) {
+    this.#fighterHealth - health < 0 ? this.#fighterHealth = 0 : this.heal(-health)
+  }
+
+  logCombatHistory() {
+    console.log(`Name:${this.getName()},Wins:${this.getWin()},Losses:${this.getLoss()}`)
+  }
+
+  attack(defender) {
+    let isSuccessful = random()
+    if ((defender.getAgility() + defender.getStrength()) > isSuccessful) {
+      console.log(`${this.getName()} attack missed`)
+    } else {
+      defender.dealDamage(this.getDamage())
+      console.log(`${this.getName()} makes ${this.getDamage()} damage to ${defender.getName()}`)
+    }
+  }
 }
 
-const battle = function (fighter1, fighter2) {
-  // add an implementation here
+function random() {
+  return Math.ceil(Math.random() * 100)
+}
+
+const battle = function (...args) {
+  let [fighter1, fighter2] = args
+  for (let i = args.length - 1; i >= 0 ; i--) {
+    if (args[i].getHealth() === 0) {
+      console.log(`${args[i].getName()} is dead`)
+      return 0
+    }
+  }
+  do {
+    fighter1.attack(fighter2)
+    fighter2.attack(fighter1)
+  } while (fighter1.getHealth() > 0 && fighter2.getHealth() > 0);
+  if (fighter1.getHealth() === 0 && fighter2.getHealth() === 0) {
+    return 0
+  }
+  if (fighter1.getHealth() === 0) {
+    console.log(`${fighter2.getName()} has won!`)
+    fighter2.addWin()
+    fighter1.addLoss()
+    return fighter1
+  } else {
+    console.log(`${fighter1.getName()} has won!`)
+    fighter1.addWin()
+    fighter2.addLoss()
+    return fighter2
+  }
 };
 
-module.exports = { Fighter, battle};
+module.exports = { Fighter, battle };
